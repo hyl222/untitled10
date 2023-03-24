47e9f8691fa437a9a70ec8ba0743191b20ee91ca
HYL <1917138161@qq.com>
2023-03-24 20:34:36 +0800

    update gg

--- src/gg.java
+++ src/gg.java
@@ -6,5 +6,6 @@ public class gg {
     public static void main(String[] args) {
         System.out.println("dasdasdasdsadasd");
         System.out.println("dasdasdasdsadasd11111111");
+        System.out.println("gfdgsfdgfsdgfd");
     }
 }
